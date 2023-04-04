0cc646dcaa049c66503a99a1ebdc0b12857b1ab1
nnp2302 <65790507+nnp2302@users.noreply.github.com>
2023-04-04 10:59:13 +0700

    Header Responsive

---
+++ Scripts/app/_MasterLayout.js
@@ -0,0 +1,12 @@
+﻿let ham_menu = document.querySelector('.ham-menu');
+let toggled_btn = document.querySelector('.toggle-btn');
+let navbar = document.querySelector('.navbar-wrap');
+
+ham_menu.onclick = function () {
+    ham_menu.classList.toggle("toggled");
+    navbar.classList.toggle("toggled");
+}
+toggled_btn.onclick = function () {
+    ham_menu.classList.toggle("toggled");
+    navbar.classList.toggle("toggled");
+}
